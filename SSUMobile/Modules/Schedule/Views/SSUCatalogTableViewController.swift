a1c96436e554bd1563c58688b6b6e2e560a7924d
Chad Vink <chad@carmelsoft.com>
2017-05-09 20:07:17 -0700

    search added

--- SSUMobile/Modules/Schedule/Views/SSUCatalogTableViewController.swift
+++ SSUMobile/Modules/Schedule/Views/SSUCatalogTableViewController.swift
@@ -7,13 +7,21 @@
 //
 
 import Foundation
+extension String {
+    func removingWhitespaces() -> String {
+        return components(separatedBy: .whitespaces).joined()
+    }
+}
 
-class SSUCatalogTableViewController: UITableViewController  {
+class SSUCatalogTableViewController: UITableViewController, UISearchResultsUpdating, UISearchBarDelegate  {
     
     var context: NSManagedObjectContext = SSUScheduleModule.instance.context!
     var backgroundImageView: UIImageView?
     private var catalog: [SSUCourse]?
+    private var filterCatalog: [SSUCourse]?
+    var searchController: UISearchController!
     
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +30,27 @@ class SSUCatalogTableViewController: UITableViewController  {
         // self.clearsSelectionOnViewWillAppear = false
         
         title = "SSU Catalog"
+        filterCatalog = []
         
         if let image = UIImage(named: "table_background_image") {
             backgroundImageView = UIImageView(image: image)
             self.tableView.backgroundView = backgroundImageView
         }
         
+        
+        
+        self.searchController = UISearchController(searchResultsController: nil)
+        self.searchController.searchResultsUpdater = self
+        self.searchController.dimsBackgroundDuringPresentation = false
+        self.searchController.searchBar.delegate = self
+        self.tableView.tableHeaderView = self.searchController.searchBar
+        self.definesPresentationContext = true
+        
+        
+        
+        UIBarButtonItem.appearance().tintColor = .white
+
+
         refresh()
     }
     
@@ -50,10 +73,10 @@ class SSUCatalogTableViewController: UITableViewController  {
     }
     
     
-    override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        self.tableView.setEditing(editing, animated: animated)
-    }
+//    override func setEditing(_ editing: Bool, animated: Bool) {
+//        super.setEditing(editing, animated: animated)
+//        self.tableView.setEditing(editing, animated: animated)
+//    }
     
     private func refresh() {
         SSUScheduleModule.instance.updateData({
@@ -71,6 +94,8 @@ class SSUCatalogTableViewController: UITableViewController  {
             catalog = []
         }
         
+        
+        
         DispatchQueue.main.async {
             self.reloadScheduleTableView()
         }
@@ -82,8 +107,6 @@ class SSUCatalogTableViewController: UITableViewController  {
     
     
     
-    
-    
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -91,7 +114,7 @@ class SSUCatalogTableViewController: UITableViewController  {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let count = catalog?.count else {
+        guard let count = filterCatalog?.count else {
             return 0
         }
         return count
@@ -101,7 +124,7 @@ class SSUCatalogTableViewController: UITableViewController  {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CourseViewCell", for: indexPath)
         
         if let theCell = cell as? SSUCourseViewCell{
-            theCell.transferClassInfo(course: (catalog![indexPath.row]))
+            theCell.transferClassInfo(course: (filterCatalog![indexPath.row]))
             // theCell.layoutMargins = UIEdgeInsets.zero
             // theCell.separatorInset = UIEdgeInsets.zero
         }
@@ -140,5 +163,82 @@ class SSUCatalogTableViewController: UITableViewController  {
         }
     }
     
+    // MARK: - UISearchResultsUpdater
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchString = searchController.searchBar.text
+        self.searchFor(text: searchString)
+        self.tableView.reloadData()
+    }
+    
+    func searchFor(text:String?){
+//        let searchTerms = text?.removingWhitespaces()
+        if (catalog == nil) {
+            return
+        }
+        if (text == ""){
+            filterCatalog = catalog
+            return
+        }
+        
+        var tempset:Set<SSUCourse>
+        var setArray:Array<Set<SSUCourse>> = []
+        for searchTerms in (text?.lowercased().components(separatedBy: .whitespaces))!{
+            if (searchTerms == ""){
+                break
+            }
+            let innerSet = Set(catalog!.filter({
+                (item:SSUCourse) in
+                return item.catalog?.lowercased().range(of:searchTerms) != nil ||
+                        item.department?.lowercased().range(of:searchTerms) != nil ||
+                        item.last_name?.lowercased().range(of:searchTerms) != nil ||
+                        item.first_name?.lowercased().range(of:searchTerms) != nil
+            }))
+            
+//            innerSet = innerSet.union(Set(catalog!.filter({
+//                (item:SSUCourse) in
+//                return item.department?.lowercased().range(of:searchTerms) != nil
+//            })))
+            
+//            innerSet = innerSet.union(Set(catalog!.filter({
+//                (item:SSUCourse) in
+//                return item.first_name?.lowercased().range(of:searchTerms) != nil
+//            })))
+//            
+//            innerSet = innerSet.union(Set(catalog!.filter({
+//                (item:SSUCourse) in
+//                return item.last_name?.lowercased().range(of:searchTerms) != nil
+//            })))
+            
+            setArray.append(innerSet)
+        }
+        
+        if(setArray.isEmpty) {
+            return
+        }
+        tempset = setArray.removeFirst()
+        for s in setArray {
+            tempset = tempset.intersection(s)
+        }
+        
+        filterCatalog = Array(tempset)
+
+        
+//        if(filterCatalog?.isEmpty)!{
+//            filterCatalog = catalog
+//        }
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        filterCatalog = catalog
+        
+    }
     
 }
